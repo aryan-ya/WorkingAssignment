e9aa260e17aafcd7397896a1f6a42acb988b005d
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-06-02 09:16:16 +0530

    Create Declarethreevariable.js

---
+++ Declarethreevariable.js
@@ -0,0 +1,14 @@
+{
+    console.log(myVariable1); 
+    let myVariable1 = "let variable";
+    var myVariable2 = "var variable";
+    const myVariable3 = "const variable";
+  
+    console.log(myVariable1); 
+    console.log(myVariable2); 
+    console.log(myVariable3); 
+  }
+  
+  console.log(myVariable2); 
+  console.log(myVariable3); 
+  
