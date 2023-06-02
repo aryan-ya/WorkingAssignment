14e6c135d61e4deab2e4e2b151851b7045184ea9
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-06-02 09:17:57 +0530

    Create Varriableletscope.js

---
+++ Varriableletscope.js
@@ -0,0 +1,8 @@
+{
+    console.log(myVariable); // Output: ReferenceError: myVariable is not defined
+  
+    let myVariable = "let variable";
+  
+    console.log(myVariable); // Output: let variable
+  }
+  
