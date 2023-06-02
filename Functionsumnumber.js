cbf88aefc1a9a64605f9dfb435638b969208ecac
Aryan yadav <107910961+aryan-ya@users.noreply.github.com>
2023-06-02 09:10:26 +0530

    Create Functionsumnumber.js

---
+++ Functionsumnumber.js
@@ -0,0 +1,11 @@
+function sumNumbers(a, b) {
+    console.log(myVariable); 
+    var myVariable;
+    console.log(myVariable); 
+    myVariable = a + b;
+    console.log(myVariable); 
+  }
+  
+  
+  sumNumbers(4, 5);
+  
